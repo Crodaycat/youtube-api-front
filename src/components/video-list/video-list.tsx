5d82ea0bb4454ba87f4c3cc2bac5ad31f9be5418
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-03-08 12:53:24 -0500

    Feat: AppContext integrated in video-list and styles for loading container

--- src/components/video-list/video-list.tsx
+++ src/components/video-list/video-list.tsx
@@ -1,23 +1,39 @@
 import './styles.css';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBack from '@material-ui/icons/ArrowBack';
 import ArrowForward from '@material-ui/icons/ArrowForward';
-import React from 'react';
+import React, { useContext } from 'react';
 import ScrollMenu from 'react-horizontal-scrolling-menu';
 
+import { AppContext } from '../app-context/app-context';
 import VideoItem from '../video-item/video-item';
 
-const videos = [
-  '/breaking-bad.webp',
-  '/the-leftovers.jpg',
-  '/game-of-thrones.jpg',
-  '/true-detective.jpg',
-  '/walking-dead.jpg'
-];
-
 export default function VideoList() {
-  const videosMenu = videos.map(video => <VideoItem key={video} />);
+  const appContext = useContext(AppContext);
+  const videosHook = appContext?.videosHook;
+
+  if (!videosHook || videosHook.status === 'iddle') {
+    return <h1>Busca videos para listar el resultado</h1>;
+  }
+
+  if (videosHook.status === 'error') {
+    return <h1>Error al consultar los videos: {videosHook.error}</h1>;
+  }
+
+  if (videosHook.status === 'loading') {
+    return (
+      <div className='loading-spinner-container'>
+        <CircularProgress size={90} thickness={5} />
+        <h4>Buscando resultados...</h4>
+      </div>
+    );
+  }
+
+  const videosMenu = videosHook.videos.map(video => (
+    <VideoItem key={video.id.videoId} />
+  ));
 
   return (
     <div>
