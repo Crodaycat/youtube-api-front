8fbebf97cc543d15c69827fdc5603d075948ec53
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-03-08 13:21:46 -0500

    Feat: nav-bar integration with AppContext and search input text logic

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -4,11 +4,31 @@ import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import logo from '../../assets/exp-logo.png';
+import { AppContext } from '../app-context/app-context';
 
 export default function NavBar() {
+  const appContext = useContext(AppContext);
+  const [searchText, setSearchText] = useState('');
+
+  function onChangeText(
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) {
+    setSearchText(event.target.value);
+  }
+
+  function onSubmit(event: React.KeyboardEvent<HTMLDivElement>) {
+    if (
+      event.key === 'Enter' &&
+      searchText &&
+      appContext?.videosHook.status !== 'loading'
+    ) {
+      appContext?.videosHook.fetchVideos(searchText);
+    }
+  }
+
   return (
     <div className='container nav-bar'>
       <div className='container'>
@@ -19,10 +39,18 @@ export default function NavBar() {
         className='search-input'
         type='search'
         variant='outlined'
+        value={searchText}
+        onChange={onChangeText}
+        onKeyPress={onSubmit}
         InputProps={{
           endAdornment: (
             <InputAdornment position='end'>
-              <IconButton aria-label='upload picture' component='span'>
+              <IconButton
+                aria-label='upload picture'
+                component='span'
+                disabled={appContext?.videosHook.status === 'loading'}
+                onClick={() => appContext?.videosHook.fetchVideos(searchText)}
+              >
                 <SearchIcon />
               </IconButton>
             </InputAdornment>
