b7e48048170224f5e00d09163eec6873d9f23381
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-03-08 23:23:36 -0500

    Feat: AppContext video selection implementation

--- src/components/app-context/app-context.tsx
+++ src/components/app-context/app-context.tsx
@@ -1,9 +1,12 @@
-import React, { createContext, ReactElement } from 'react';
+import React, { createContext, ReactElement, useState, useEffect } from 'react';
 
 import { useGetVideos, useGetVideosType } from '../../hooks/use-get-videos';
+import { YouTuBeVideo } from '../../models/youtube-video';
 
 interface IAppContext {
   videosHook: useGetVideosType;
+  selectedVideo: YouTuBeVideo | null;
+  setSelectedVideo: (video: YouTuBeVideo | null) => void;
 }
 
 export const AppContext = createContext<IAppContext | null>(null);
@@ -13,10 +16,25 @@ interface IProps {
 }
 
 export function AppProvider(props: IProps) {
-  const videosHook = useGetVideos();
+  const { status, videos, error, fetchVideos } = useGetVideos();
+  const [selectedVideo, setSelectedVideo] = useState<YouTuBeVideo | null>(null);
+
+  useEffect(() => {
+    if (status === 'loading') {
+      setSelectedVideo(null);
+    } else if (status === 'success' && videos.length > 0) {
+      setSelectedVideo(videos[0]);
+    }
+  }, [status, videos]);
 
   return (
-    <AppContext.Provider value={{ videosHook }}>
+    <AppContext.Provider
+      value={{
+        videosHook: { status, videos, error, fetchVideos },
+        selectedVideo,
+        setSelectedVideo
+      }}
+    >
       {props.children}
     </AppContext.Provider>
   );
